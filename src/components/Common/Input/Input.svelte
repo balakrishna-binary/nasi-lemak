e0d231e99977a95aa05c89ca65cbece93ec375b3
Sandeep <sandeep@deriv.com>
2022-02-09 10:11:04 +0800

    Componentizing Elements for ease

---
+++ src/components/Common/Input/Input.svelte
@@ -0,0 +1,14 @@
+<script>
+  export let id, placeholder, label, value;
+</script>
+
+<div class="form__group mr-1">
+  <input
+    type="text"
+    class="form__input width-100"
+    id={id}
+    placeholder={placeholder}
+    bind:value={value}
+  />
+  <label for="name" class="form__label">{label}</label>
+</div>
